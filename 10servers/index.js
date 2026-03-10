b1a52721dd93f8db85909935be4de0e79ec4e977
Patel Jivan <patel.jivan.cg@gmail.com>
2026-03-10 12:19:06 +0530

    Query parameters for brand and categure done

--- 10servers/index.js
+++ 10servers/index.js
@@ -13,6 +13,10 @@ const productSchema = mongoose.Schema({
         type: String,
         required: [true, "Title of product is required"]
     },
+    "brand": {
+        type: String,
+        required: [true, "Brand of product is required"]
+    },
     "price": {
         type: Number,
         required: [true, "Price of product is required"]
@@ -45,6 +49,76 @@ app.post("/products", async (req, res) => {
     }
 })
 
+app.get("/products/id/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            return req.status(404).send("Product not found");
+        }
+        res.status(200).json(product);
+    }
+    catch (error) {
+        res.status(404).json(error);
+    }
+})
+
+app.get("/get-products", async (req, res) => {
+    try {
+        const products = await Product.find();
+        if (!products) {
+            return req.status(404).send("Product not found");
+        }
+        res.status(200).json(products);
+    }
+    catch (error) {
+        res.status(404).json(error);
+    }
+})
+
+// Query Parameters
+
+app.get("/categoryProducts", async (req, res) => {
+    try {
+        const category = req.query;
+        console.log(category)
+        const products = await Product.find(category)
+        res.status(200).json(products);
+    }
+    catch (error) {
+        res.status(404).json(error);
+    }
+})
+
+app.get("/brandProducts", async (req, res) => {
+    try {
+        const brand = req.query.brand;
+        const products = await Product.find({ "brand": brand })
+        res.status(200).json(products);
+    }
+    catch (error) {
+        res.status(404).json(error);
+    }
+})
+
+
+app.get("/products", async (req, res) => {
+    try {
+        const { brand, category } = req.query;
+        let filter = {};
+        if(brand) {
+            filter.brand = brand;
+        }
+        if(category) {
+            filter.category = category;
+        }
+        const products = await Product.find(filter)
+        res.status(200).json(products);
+    }
+    catch (error) {
+        res.status(404).json(error);
+    }
+})
 
 app.listen(3000, () => {
     console.log("server started at 3000")
